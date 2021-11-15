78cbe97d8e8dea8a307bff0d5b7ee6a38fe3d6a2
aliakbarkhan <90334983+aliakbarkhan@users.noreply.github.com>
2021-11-15 18:44:10 +0530

    Add files via upload

---
+++ form handler.php
@@ -0,0 +1,27 @@
+<?php
+$name= $_POST['name'];
+$visitor_email= $_POST['email'];
+$subject= $_POST['subject'];
+$message= $_POST['message'];
+
+$email_from="schizophrenia survey.com";
+
+$email_subject='New Form Submission';
+
+$emil_body="Username:$name.\n".
+    "User email:$visitor_email.\n".
+    "Subject:$subject.\n"
+    "User message:$message.\n";
+$to ='ali.khan20@vit.edu';
+
+
+$headers ="from:$email_from \r\n";
+
+$headers .="reply to:$visitor_email \r\n";
+
+
+mail($to,$email_subject,$emil_body,$headers);
+
+header("location: contact.html");
+
+?>
